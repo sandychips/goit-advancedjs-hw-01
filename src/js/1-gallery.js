dec452405da49f0d2fa81517b202fb3be741d8c4
Chips <149303347+sandychips@users.noreply.github.com>
2024-12-09 13:20:00 +0200

    SRC Dir and package.json update

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,33 +1,95 @@
-import images from './imagesData.json';
-import { createGalleryImagesTemplate } from './render-functions';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryList = document.querySelector(".gallery")
-galleryList.innerHTML = createGalleryImagesTemplate(images);
+window.global = window;
 
-galleryList.style.listStyleType = "none";
-galleryList.style.width = "1128px";
-galleryList.style.height = "648px";
-galleryList.style.padding = "24px 156px";
-galleryList.style.display = "grid";
-galleryList.style.gridTemplateColumns = "repeat(3, 1fr)";
-galleryList.style.gap = "24px";
-galleryList.style.background = "#FFF";
-galleryList.style.position = "relative";
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/ landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
 
-new SimpleLightbox('.gallery a', {
+// Gallery element layout
+const galleryList = document.querySelector('.gallery');
+
+images.forEach(item => {
+  const galleryItemHTML = `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${item.original}">
+          <img
+            class="gallery-image"
+            src="${item.preview}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>
+    `;
+  galleryList.insertAdjacentHTML('beforeend', galleryItemHTML);
+});
+
+// Initialize SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
   captionsData: 'alt',
-  captionDelay: 250
-})
-
-const galleryItems = document.querySelectorAll(".gallery-item")
-galleryItems.forEach(galleryItem => {
-    galleryItem.style.width = "360px";
-    galleryItem.style.height = "200px";
-
-    const img = galleryItem.querySelector("img");
-    if (img) {
-        img.style.width = "360px";
-        img.style.height = "200px";
-    }
-})
+  captionDelay: 250,
+});
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,142 +1,45 @@
-import { save, load } from "./localStorage";
-import iziToast from "izitoast";
-
-document.body.style.fontFamily = "'Montserrat', sans-serif";
-const form = document.querySelector("form");
-const labels = document.querySelectorAll("label");
-const input = document.querySelector("input");
-const textarea = document.querySelector("textarea");
-const submitButton = document.querySelector("button")
-
+// An object for storing form data
 const formData = {
     email: '',
     message: '',
-};
-
-const fillFormField = () => {
-    const formDataFromLS = load('feedback-form-state');
-
-    if (formDataFromLS === undefined) {
-        return;
+  };
+  
+  // Form
+  const form = document.querySelector('.feedback-form');
+  
+  // Checking local storage when loading a page
+  window.addEventListener('load', () => {
+    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (savedData) {
+      formData.email = savedData.email || '';
+      formData.message = savedData.message || '';
+      form.elements.email.value = formData.email;
+      form.elements.message.value = formData.message;
     }
-
-    const formDataFromLSKeys = Object.keys(formDataFromLS);
-
-    formDataFromLSKeys.forEach(key => {
-        form.elements[key].value = formDataFromLS[key];
-        formData[key] = formDataFromLS[key];
-    });
-
-  console.log(formData);
-};
-
-fillFormField();
-
-const onFormFieldChange = event => {
-    const { target: formField } = event;
-
-    const fieldName = formField.name;
-    const fieldValue = formField.value;
-
-    formData[fieldName] = fieldValue;
-
-    save('feedback-form-state', formData);
-};
-
-const onFeedbackFormSubmit = event => {
+  });
+  
+  // Delegation of events for input tracking
+  form.addEventListener('input', event => {
+    const { name, value } = event.target;
+    formData[name] = value.trim(); // We save data without spaces
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData)); // Save to local storage
+  });
+  
+  // Form submission processing
+  form.addEventListener('submit', event => {
     event.preventDefault();
-
-    const formDataValues = Object.values(formData);
-
-    if (formDataValues.some(el => el === '')) {
-        iziToast.error({
-        message: 'Fill please all fields',
-        position: 'topRight',
-        });
-        return;
+  
+    // Check that the fields are filled in
+    if (formData.email === '' || formData.message === '') {
+      alert('Fill please all fields');
+      return;
     }
-    
+  
+    // Data output to the console
     console.log(formData);
-
-    event.currentTarget.reset();
+  
+    // Cleanup of local storage and forms
     localStorage.removeItem('feedback-form-state');
-};
-
-form.addEventListener('input', onFormFieldChange);
-form.addEventListener('submit', onFeedbackFormSubmit);
-
-form.style.display = "inline-flex";
-form.style.padding = "24px";
-form.style.flexDirection = "column";
-form.style.gap = "8px";
-form.style.borderRadius = "8px";
-form.style.background = "#fff";
-
-labels.forEach(label => {
-    label.style.display = "flex";
-    label.style.flexDirection = "column";
-    label.style.gap = "8px";
-    label.style.color = "#2e2f42";
-    label.style.fontFamily = "'Montserrat', sans-serif";
-    label.style.fontSize = "16px";
-    label.style.fontStyle = "normal";
-    label.style.fontWeight = "400";
-    label.style.lineHeight = "24px";
-    label.style.letterSpacing = "0.64px";
-})
-
-input.style.outline = "none";
-input.style.width = "360px";
-input.style.height = "40px";
-input.style.borderRadius = "4px";
-input.style.border = "1px solid #808080";
-input.style.padding = "8px 16px";
-input.style.fontFamily = "'Montserrat', sans-serif";
-
-textarea.style.outline = "none";
-textarea.style.width = "360px";
-textarea.style.height = "80px";
-textarea.style.borderRadius = "4px";
-textarea.style.border = "1px solid #808080";
-textarea.style.padding = "8px 16px";
-textarea.style.fontFamily = "'Montserrat', sans-serif";
-
-input.addEventListener("mouseenter", () => {
-    input.style.border = "1px solid #000";
-});
-input.addEventListener("mouseleave", () => {
-    input.style.border = "1px solid #808080";
-});
-
-textarea.addEventListener("mouseenter", () => {
-    textarea.style.border = "1px solid #000";
-});
-textarea.addEventListener("mouseleave", () => {
-    textarea.style.border = "1px solid #808080";
-});
-
-submitButton.style.outline = "none";
-submitButton.style.marginTop = "8px";
-submitButton.style.width = "86px";
-submitButton.style.height = "40px";
-submitButton.style.display = "flex";
-submitButton.style.justifyContent = "center";
-submitButton.style.alignItems = "center";
-submitButton.style.padding = "8px 16px";
-submitButton.style.borderRadius = "8px";
-submitButton.style.border = "none";
-submitButton.style.background = "#4E75FF";
-submitButton.style.color = "#FFF";
-submitButton.style.fontFamily = "'Montserrat', sans-serif";
-submitButton.style.fontSize = "16px";
-submitButton.style.fontStyle = "normal";
-submitButton.style.fontWeight = "500";
-submitButton.style.lineHeight = "24px";
-submitButton.style.letterSpacing = "0.64px";
-
-submitButton.addEventListener("mouseenter", () => {
-    submitButton.style.background = "#6C8CFF";
-});
-submitButton.addEventListener("mouseleave", () => {
-    submitButton.style.background = "#4E75FF";
-});
+    form.reset();
+    Object.keys(formData).forEach(key => (formData[key] = '')); // Reset the formData object
+  });
